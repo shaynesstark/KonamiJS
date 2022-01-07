2e18798a21022b4889533bc80728925d6302ab90
Shayne Stark <shaynesstark@gmail.com>
2022-01-07 13:25:50 -0500

    moved acceptedArray outside keydown event

--- konami.js
+++ konami.js
@@ -1,53 +1,51 @@
 const addEvent = (evnt, elem, func) => {
-   if (elem.addEventListener) elem.addEventListener(evnt,func,false);
-   else if (elem.attachEvent) elem.attachEvent("on"+evnt, func);
-   else elem["on"+evnt] = func;
-}
+    if (elem.addEventListener) elem.addEventListener(evnt, func, false);
+    else if (elem.attachEvent) elem.attachEvent('on' + evnt, func);
+    else elem['on' + evnt] = func;
+};
 
 const arraysEqual = (a, b) => {
-	if (a === b) return true;
-	if (a.length !== b.length) return false;
+    if (a === b) return true;
+    if (a.length !== b.length) return false;
 
-	for (var i = 0; i < a.length; i++) {
-		if (a[i] !== b[i]) return false;
-	}
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) return false;
+    }
 
-	return true;
+    return true;
 };
 
 const konami = () => {
-	let buffer = [];
-	let lastKeyTime = Date.now();
-	const charList = ['b', 'a', 'arrowup', 'arrowdown', 'arrowright', 'arrowleft'];
+    let buffer = [];
+    let lastKeyTime = Date.now();
+    const charList = ['b', 'a', 'arrowup', 'arrowdown', 'arrowright', 'arrowleft'];
+    const acceptedArray = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a'];
 
-	addEvent('keydown', document, event => {
+    addEvent('keydown', document, (event) => {
+        const key = event.key.toLowerCase();
 
-		const key = event.key.toLowerCase();
+        // we are only interested in keys that are part of the konami code
+        if (charList.indexOf(key) === -1) return;
 
-		// we are only interested in keys that are part of the konami code
-		if (charList.indexOf(key) === -1) return;
+        const currentTime = Date.now();
 
-		const currentTime = Date.now();
+        // Reset buffer array if user didn't enter a sequential key within 0.5 seconds
+        if (currentTime - lastKeyTime > 500) buffer = [];
 
-		// Reset buffer array if user didn't enter a sequential key within 0.5 seconds
-		if (currentTime - lastKeyTime > 500) buffer = [];
+        // Reset buffer if it is longer that the konami code
+        if (buffer.length > 10) buffer = [];
 
-		// Reset buffer if it is longer that the konami code
-		if (buffer.length > 10) buffer = [];
+        buffer.push(key);
+        lastKeyTime = currentTime;
 
-		buffer.push(key);
-		lastKeyTime = currentTime;
-
-		const acceptedArray = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a'];
-		
-		if (arraysEqual(buffer, acceptedArray)) {
-			// User has entered the konami code do something cool!
-			console.log('Konami code entered!')
-		}
-	});
+        if (arraysEqual(buffer, acceptedArray)) {
+            // User has entered the konami code do something cool!
+            console.log('Konami code entered!');
+        }
+    });
 };
 
 addEvent('DOMContentLoaded', document, () => {
-	'use strict';
-	konami();
+    'use strict';
+    konami();
 });
